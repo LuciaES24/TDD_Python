b7f18f2bc790ebe437382f0c976586a0064b62cc
LuciaES24 <lespsan543@g.educaand.es>
2024-10-21 12:02:34 +0200

    añadidos test Lucia

---
+++ test_lucia_3110.py
@@ -0,0 +1,9 @@
+def test_menor_numero():
+    menor = 8
+    lista = [8,10,12,31]
+    assert menor_numero_encontrado(lista) == menor
+
+def test_mayor_numero():
+    mayor = 80
+    lista = [12,80,33,43]
+    assert mayor_numero_encontrado(lista) == mayor 
---
+++ test_lucia_318.py
@@ -0,0 +1,5 @@
+def test_palindromo():
+    palabra1 = "hola"
+    palabra2 = "uwu"
+    assert es_palindromo(palabra1) == False
+    assert es_palindromo(palabra2) == True
